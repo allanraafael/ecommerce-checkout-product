7358908f011179b15415d6cce198d23ec147b60f
Allan Rafael <allanrafaelfo@gmail.com>
2021-08-27 23:48:24 -0300

    Feito leitura de arquivo JSON para backend

--- product/product.go
+++ product/product.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+
+func loadData() []byte {
+	jsonFile, err := os.Open("products.json")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer jsonFile.Close()
+
+	data, err := ioutil.ReadAll(jsonFile)
+	
+	return data
+}
 
 
 func main() {
-	fmt.Println("Olá, tudo bem?")
+	fmt.Println(string(loadData()))
 }
